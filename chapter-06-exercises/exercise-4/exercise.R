68550fee447ad177acc7add69e1f36379e3d76ef
Liothen <liothen@flagrun.net>
2020-09-29 09:52:35 -0700

    homework

--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -4,21 +4,34 @@
 # returns whether or not (e.g., a boolean) the length of one argument is greater
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
-
+is_twice_as_long <- function(a, b){
+  nchar(a) >= (nchar(b) * 2)
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
-
-
+is_twice_as_long("test", "te")
+is_twice_as_long("lol", "lol")
+is_twice_as_long("roflcopter", "lol")
 
 # Define a function `describe_difference` that takes in two strings. The
 # function should return one of the following sentences as appropriate
 #   "Your first string is longer by N characters"
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
-
+describe_difference <- function(a, b){
+  if(nchar(a) == nchar(b)){
+    print("Your strings are the same length!")
+  }else if(nchar(a) > nchar(b)){
+    print(paste("Your first string is longer by", (nchar(a) - nchar(b)), "characters"))
+  }else {
+    print(paste("Your second string is longer by", (nchar(b) - nchar(a)), "characters"))
+  }
+}
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
-
+describe_difference("test", "te")
+describe_difference("lol", "lol")
+describe_difference("lol", "roflcopter")
--- chapter-07-exercises/exercise-3/exercise.R
+++ chapter-07-exercises/exercise-3/exercise.R
@@ -2,30 +2,33 @@
 
 # Create a vector `words` of 6 (or more) words.
 # You can Google for a "random word generator" if you wish!
-
+words <- c("beware", "oblivion", "is", "at", "hand", "power", "overwhelming")
 
 # Create a vector `words_of_the_day` that is your `words` vector with the string
 # "is the word of the day!" pasted on to the end.
 # BONUS: Surround the word in quotes (e.g., `'data' is the word of the day!`)
 # Note that the results are more obviously correct with single quotes.
-
+words_of_the_day <- paste("'",sample(words,1),"' is the word of the day", sep='')
 
 # Create a vector `a_f_words` which are the elements in `words` that start with 
 # "a" through "f"
 # Hint: use a comparison operator to see if the word comes before "f" alphabetically!
 # Tip: make sure all the words are lower-case, and only consider the first letter
 # of the word!
-
+a_f_words <- words[substring(words,1,1) %in% letters[1:6]]
 
 # Create a vector `g_m_words` which are the elements in `words` that start with 
 # "g" through "m"
-
+g_m_words <- words[substring(words,1,1) %in% letters[7:13]]
 
 # Define a function `word_bin` that takes in three arguments: a vector of words, 
 # and two letters. The function should return a vector of words that go between 
 # those letters alphabetically.
-
+word_bin <- function(list_of_words, start, end){
+  list_of_words[list_of_words>= start & substring(list_of_words, 1, 1)<=end]
+}
 
 # Use your `word_bin` function to determine which of your words start with "e" 
 # through "q"
+word_bin(words, 'e', 'q')
 
